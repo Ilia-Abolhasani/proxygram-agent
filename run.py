1a71283ac3b42a1513ea5cc4d6bfb1e93700bbbb
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2024-05-23 17:37:38 +0330

    use proxies-tg-wrapper package

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import src
 from src.server import Server
-from telegram.telegram_api import Telegram_API
+from proxies_tg_wrapper.api_wrapper import Telegram_API
 from src.cron import manager
 from src.config import Config
 
