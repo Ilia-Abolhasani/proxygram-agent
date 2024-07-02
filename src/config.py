5ab3fc922f4b259daaa9511443fa948fe3bf7dfc
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2024-07-02 14:54:29 +0330

    lib path added to config.

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
@@ -15,17 +16,15 @@ class Config:
     start_mtproto_secret = os.getenv("start_mtproto_secret")
     database_encryption_key = os.getenv("database_encryption_key")
     tdlib_directory_ping = os.getenv("tdlib_directory_ping")
-    tdlib_lib_path_ping = None #os.getenv("tdlib_lib_path_ping")
+    tdlib_lib_path_ping = os.getenv("tdlib_lib_path_ping")
     tdlib_directory_speed = os.getenv("tdlib_directory_speed")
-    tdlib_lib_path_speed = None #os.getenv("tdlib_lib_path_speed")
+    tdlib_lib_path_speed = os.getenv("tdlib_lib_path_speed")
     download_timeout = int(os.getenv("download_timeout"))
     download_chat_id = os.getenv("download_chat_id")
     download_username = os.getenv("download_username")
     download_message_id = os.getenv("download_message_id")
-    cron_expression_speed_test = os.getenv(
-        "cron_expression_speed_test")
+    cron_expression_speed_test = os.getenv("cron_expression_speed_test")
     cron_expression_ping = os.getenv("cron_expression_ping")
-    cron_expression_ping_disconnect = os.getenv(
-        "cron_expression_ping_disconnect")
+    cron_expression_ping_disconnect = os.getenv("cron_expression_ping_disconnect")
     batch_size_ping = int(os.getenv("batch_size_ping"))
     batch_size_speed_test = int(os.getenv("batch_size_speed_test"))
