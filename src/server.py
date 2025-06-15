86af0105fd71745c08ffa18dfdb4440141a9ddcf
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2025-06-15 00:36:22 -0700

    soft delete function added to server

--- src/server.py
+++ src/server.py
@@ -12,35 +12,34 @@ class Server:
         self.agent_secret = Config.agent_secret
 
     def _create_headers(self):
-        timestamp = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
-        message = f'{timestamp}{self.agent_secret}'
+        timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+        message = f"{timestamp}{self.agent_secret}"
         hashed_timestamp = hashlib.sha256(message.encode()).hexdigest()
 
-        headers = {
-            'X-Request-Time': timestamp,
-            'X-Hashed-Timestamp': hashed_timestamp
-        }
+        headers = {"X-Request-Time": timestamp, "X-Hashed-Timestamp": hashed_timestamp}
         return headers
 
     def _get(self, url, query, sendLog=True):
         headers = self._create_headers()
         response = None
         try:
-            response = requests.get(self.base_url + url,
-                                    params=query, headers=headers, timeout=5)
+            response = requests.get(
+                self.base_url + url, params=query, headers=headers, timeout=5
+            )
             if response.status_code == 200:
                 data = response.json()
                 return data
             raise Exception("Status_code is not 200.")
-        except Exception as err:       
-            msg = f'''Request failed with status code: {response.status_code if response else ""}
+        except Exception as err:
+            msg = f"""Request failed with status code: {response.status_code if response else ""}
                 Method: GET
                 Response: {response.text if response else ""}
                 Url: {url}
                 Query: {query}
-                Exception: {err}'''
+                Exception: {err}"""
             print(msg)
-            if sendLog: self.send_log(msg)
+            if sendLog:
+                self.send_log(msg)
         return None
 
     def _post(self, url, query, body, sendLog=True):
@@ -48,45 +47,49 @@ class Server:
         response = None
         try:
             response = requests.post(
-                self.base_url + url, params=query, json=body, headers=headers, timeout=5)
+                self.base_url + url, params=query, json=body, headers=headers, timeout=5
+            )
             if response.status_code == 201 or response.status_code == 200:
                 data = response.json()
                 return data
             raise Exception("Status_code is not 200.")
         except Exception as err:
-            msg = f'''
+            msg = f"""
                 Request failed with status code: {response.status_code if response else ""}
                 Method: POST
                 Response: {response.text if response else ""}
                 Url: {url}
                 Query: {query}
                 Body: {body}
-            '''
+            """
             print(msg)
-            if sendLog: self.send_log(msg)
+            if sendLog:
+                self.send_log(msg)
         return None
 
     def _update(self, url, query, body, sendLog=True):
         headers = self._create_headers()
         response = None
         try:
-            response = requests.put(self.base_url + url,
-                                    params=query, json=body, headers=headers, timeout=5)
+            response = requests.put(
+                self.base_url + url, params=query, json=body, headers=headers, timeout=5
+            )
             if response.status_code == 200:
                 data = response.json()
                 return data
             raise Exception("Status_code is not 200.")
         except Exception as err:
-            msg = f'''
+            msg = f"""
                 Request failed with status code: {response.status_code if response else ""}
                 Method: PUT
                 Response: {response.text if response else ""}
                 Url: {url}
                 Query: {query}
                 Body: {body}
-            '''
+            """
             print(msg)
-            if sendLog: self.send_log(msg)
+            if sendLog:
+                self.send_log(msg)
         return None
 
     def _delete(self, url, query, sendLog=True):
@@ -94,21 +97,25 @@ class Server:
         response = None
         try:
             response = requests.delete(
-                self.base_url + url, params=query, headers=headers, timeout=5)
-            if response.status_code == 204:  # Assuming 204 is the successful status code for a successful deletion
+                self.base_url + url, params=query, headers=headers, timeout=5
+            )
+            if (
+                response.status_code == 204
+            ):  # Assuming 204 is the successful status code for a successful deletion
                 print("Resource deleted successfully.")
                 return
             raise Exception("Status_code is not 204.")
         except Exception as err:
-            msg = f'''
+            msg = f"""
                 Request failed with status code: {response.status_code if response else ""}
                 Method: DELETE
                 Response: {response.text if response else ""}
                 Url: {url}
                 Query: {query}                
-            '''
+            """
             print(msg)
-            if sendLog: self.send_log(msg)        
+            if sendLog:
+                self.send_log(msg)
 
     def get_ping_proxies(self, disconnect):
         query = {"disconnect": disconnect}
@@ -132,9 +139,11 @@ class Server:
         query = {}
         return self._delete(f"/api/{self.agent_id}/proxy/delete/{proxy_id}", query)
 
+    def soft_delete_proxy(self, proxy_id):
+        query = {}
+        return self._delete(f"/api/{self.agent_id}/proxy/delete/soft/{proxy_id}", query)
+
     def send_log(self, message):
         query = {}
-        body = {
-            "message": message
-        }        
+        body = {"message": message}
         return self._post(f"/api/{self.agent_id}/log/recive", query, body, False)
