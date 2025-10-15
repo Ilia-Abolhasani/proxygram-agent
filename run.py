a46c875f2dba99d5d4b4d5b0f0b7d23a4baf7b9b
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2025-10-14 22:31:54 -0700

    use same credentional

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from src.cron import manager
 from src.config import Config
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     server = Server()
     # telegram api for ping
     telegram_api_ping = Telegram_API(
@@ -17,22 +17,23 @@ if __name__ == '__main__':
         Config.tdlib_lib_path_ping,
         Config.start_mtproto_address,
         Config.start_mtproto_port,
-        Config.start_mtproto_secret
+        Config.start_mtproto_secret,
     )
     telegram_api_ping.set_log_verbose_level(1)
     result = telegram_api_ping.remove_all_proxies()
     # telegram api for speed
-    telegram_api_speed = Telegram_API(
-        Config.telegram_app_id,
-        Config.telegram_app_hash,
-        Config.telegram_phone,
-        Config.database_encryption_key,
-        Config.tdlib_directory_speed,
-        Config.tdlib_lib_path_speed,
-        Config.start_mtproto_address,
-        Config.start_mtproto_port,
-        Config.start_mtproto_secret
-    )
+    # telegram_api_speed = Telegram_API(
+    #     Config.telegram_app_id,
+    #     Config.telegram_app_hash,
+    #     Config.telegram_phone,
+    #     Config.database_encryption_key,
+    #     Config.tdlib_directory_ping,
+    #     Config.tdlib_lib_path_ping,
+    #     Config.start_mtproto_address,
+    #     Config.start_mtproto_port,
+    #     Config.start_mtproto_secret,
+    # )
+    telegram_api_speed = telegram_api_ping
     telegram_api_speed.set_log_verbose_level(1)
     result = telegram_api_speed.remove_all_proxies()
     print("strat manager.")
